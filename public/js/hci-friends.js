32f8915bb89e4646d7bdafbf13f8261f9bebae52
Alexandra Huang <a5huang@ucsd.edu>
2015-02-06 01:25:08 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$(".name").click(function(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
+})
+
 /*
  * Function that is called when the document is ready.
  */
